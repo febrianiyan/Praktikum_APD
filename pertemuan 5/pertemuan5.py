f4584ea638f7fdcc10d080f70d89382bd96e7f4f
febrianiyan <febrian020608@gmail.com>
2024-10-04 21:20:25 +0800

    Add files via upload

---
+++ pertemuan 5/pertemuan5.py
@@ -0,0 +1,76 @@
+# list1=["a",2,True,[1,2,3]]
+
+# print (list1[-1]
+
+
+# tas=["buku",32,True,3.14,["if",24]]
+
+# for i in range(len(tas)):
+#                print(tas[i])
+
+
+# #list mata kuliah yang sulit di semester 1
+# matkul = ['Kalkulus', 'Fisika Dasar', 'Pengantar Teknologi Informasi']
+# print(matkul)
+
+# #tampilkan list sebelum di append dengan elemen baru
+# #kita akan menambahkan mata kuliah “Logika Informatika” ke dalam list di atas
+# matkul.append('Logika Informatika')
+# print(matkul)
+
+# matkul.insert(3,"analogi")
+# print(matkul)
+
+# matkul[3]="pti"
+# print(matkul)
+
+# del matkul [4]
+# print (matkul)
+
+# prodi=["informatika","sistem informasi","teknik arsitektur","teknik lingkungan",
+#        "teknik pertambangan","teknik electro","teknik industri","teknik sipil","teknik geologi","teknik kimia"]
+
+# print(prodi[::2]."\n")
+
+# hewan=["gajah","harimau","semut"]
+# print(hewan)
+# buah=["apel","semangka"]
+# print(buah)
+# gabungan=hewan+buah
+# print(gabungan)
+
+# barang=[["sepatu","tas","baju"],["pulpen","pensil","laptop"]]
+# # print(barang[-1][-2])
+
+# print(barang)
+# for i in barang:
+#     for j in i:
+#         print(j)
+
+# nama="aldi","daffa","reza"
+# nama.append('rava')
+# print(nama)
+
+# barang1=["baju","tas","sepatu"]
+# barang2=["sepeda","motor","mobil"]
+
+
+
+
+# #mendeklarasikan tuple
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama = mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# print(mahasiswa)
+# mahasiswa=list(mahasiswa)
+# mahasiswa[2]="2409106090"
+# mahasiswa=tuple(mahasiswa)
+# print(mahasiswa)

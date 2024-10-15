9b2ebb7bc2d1d66c2509a98a45e48ae67543d853
febrianiyan <febrian020608@gmail.com>
2024-10-15 15:22:03 +0800

    Delete Post-Test/Post-Test 2/tugas directory

--- Post-Test/Post-Test 2/tugas/2409106090_muhammad_febriasnyah_B2'24_postes2.py
+++
@@ -1,9 +0,0 @@
-A1=str(input("makanan "))
-A2=float(input("masukkan harga " ))
-A3=int(input("berapa yang mau di beli "))
-A4=float(input("diskon nya itu sebesar 90% ketik 90 jika ingin melanjutkam "))
-AB=A3*A2
-AC=AB*0.90
-AD=AB-AC
-AE=90%3
-print("anda membeli",A3,A1,"dengan harga satuan",A2,"harga sebelum diskon",AB,"total diskon adalah",AC,"dan total yang harus di bayar sebesar",AD,"90 di bagi dengan 3 sisanya",AE)
